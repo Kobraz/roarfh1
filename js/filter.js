055e9e072804be0c431e135367bebe13ee0a1f6b
Kobraz <roar.f.hanssen@gmail.com>
2023-03-10 14:28:06 +0100

    div

--- js/filter.js
+++ js/filter.js
@@ -1,26 +1,24 @@
 $(document).ready(function(){
-    var activeCat = "";
-    function filterGroup(group){
-      if(activeCat != group){
-        $("div").filter("."+group).show();
-        $("div").filter(":not(."+group+")").hide();
-        activeCat = group;
-      }
+  var activeGenre = "";
+  function filtergen(gen){
+    if(activeGenre != gen){
+      $("div").filter("."+gen).show();
+      $("div").filter(":not(."+gen+")").hide();
+      activeGenre = gen;
     }
-    
-    $(".cat-all").click(function(){
-      $("div").show();
-      activeCat = "all";
-    });
-    $(".cat-1").click(function(){ filterGroup("group-1"); });
-    $(".cat-2").click(function(){ filterGroup("group-2"); });
-    $(".cat-3").click(function(){ filterGroup("group-3"); });
-    $(".cat-4").click(function(){ filterGroup("group-4"); });
-    $(".cat-5").click(function(){ filterGroup("group-5"); });
-    $(".cat-6").click(function(){ filterGroup("group-6"); });
-    $(".cat-7").click(function(){ filterGroup("group-7"); });
-    $(".cat-8").click(function(){ filterGroup("group-8"); });
-    $(".cat-9").click(function(){ filterGroup("group-9"); });
-    $(".cat-10").click(function(){ filterGroup("group-10"); });
-    $(".cat-11").click(function(){ filterGroup("group-11"); });
-  });
+  }
+  
+  
+  $(".genre-0").click(function(){ filtergen("gen-0"); activeGenre = "gen-0"; });
+  $(".genre-1").click(function(){ filtergen("gen-1"); });
+  $(".genre-2").click(function(){ filtergen("gen-2"); });
+  $(".genre-3").click(function(){ filtergen("gen-3"); });
+  $(".genre-4").click(function(){ filtergen("gen-4"); });
+  $(".genre-5").click(function(){ filtergen("gen-5"); });
+  $(".genre-6").click(function(){ filtergen("gen-6"); });
+  $(".genre-7").click(function(){ filtergen("gen-7"); });
+  $(".genre-8").click(function(){ filtergen("gen-8"); });
+  $(".genre-9").click(function(){ filtergen("gen-9"); });
+  $(".genre-10").click(function(){ filtergen("gen-10"); });
+  $(".genre-11").click(function(){ filtergen("gen-11"); });
+});

3ff174c3e66c747681de0c35bdfc3b3bdeae916f
Kobraz <roar.f.hanssen@gmail.com>
2023-03-17 18:14:13 +0100

    div

--- js/filter.js
+++ js/filter.js
@@ -1,24 +1,28 @@
-$(document).ready(function(){
-  var activeGenre = "";
-  function filtergen(gen){
-    if(activeGenre != gen){
-      $("div").filter("."+gen).show();
-      $("div").filter(":not(."+gen+")").hide();
-      activeGenre = gen;
+
+const filters = document.querySelectorAll('.filter');
+
+filters.forEach(filter => { 
+
+  filter.addEventListener('click', function() {
+
+    let selectedFilter = filter.getAttribute('data-filter');
+    let itemsToHide = document.querySelectorAll(`.projects .project:not([data-filter='${selectedFilter}'])`);
+    let itemsToShow = document.querySelectorAll(`.projects [data-filter='${selectedFilter}']`);
+
+    if (selectedFilter == 'all') {
+      itemsToHide = [];
+      itemsToShow = document.querySelectorAll('.projects [data-filter]');
     }
-  }
-  
-  
-  $(".genre-0").click(function(){ filtergen("gen-0"); activeGenre = "gen-0"; });
-  $(".genre-1").click(function(){ filtergen("gen-1"); });
-  $(".genre-2").click(function(){ filtergen("gen-2"); });
-  $(".genre-3").click(function(){ filtergen("gen-3"); });
-  $(".genre-4").click(function(){ filtergen("gen-4"); });
-  $(".genre-5").click(function(){ filtergen("gen-5"); });
-  $(".genre-6").click(function(){ filtergen("gen-6"); });
-  $(".genre-7").click(function(){ filtergen("gen-7"); });
-  $(".genre-8").click(function(){ filtergen("gen-8"); });
-  $(".genre-9").click(function(){ filtergen("gen-9"); });
-  $(".genre-10").click(function(){ filtergen("gen-10"); });
-  $(".genre-11").click(function(){ filtergen("gen-11"); });
+
+    itemsToHide.forEach(el => {
+      el.classList.add('hide');
+      el.classList.remove('show');
+    });
+
+    itemsToShow.forEach(el => {
+      el.classList.remove('hide');
+      el.classList.add('show'); 
+    });
+
+  });
 });
